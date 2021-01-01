5fdb8a42935429e601660a2f526d9089ca6f5961
Ilya Petrenko <gr0x562@gmail.com>
2021-01-01 23:15:56 +0300

    refactoring tests

--- arrayAndSlice/arrayAndSlice_test.go
+++ arrayAndSlice/arrayAndSlice_test.go
@@ -7,15 +7,20 @@ import (
 
 func TestSum(t *testing.T) {
 
+	checkSum := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	}
+
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1,2,3,4,5}
 
 		got := Sum(numbers)
 		want := 15
-	
-		if got != want {
-			t.Errorf("got %d want %d given %v", got, want, numbers)
-		}
+
+		checkSum(t, got, want, numbers)
 	})
 	
 	t.Run("collection of any size", func(t *testing.T) {
@@ -23,10 +28,8 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		want := 6
-	
-		if got != want {
-			t.Errorf("got %d want %d given %v", got, want, numbers)
-		}
+		
+		checkSum(t, got, want, numbers)
 	})
 }
 
@@ -41,21 +44,22 @@ func TestSumAll(t *testing.T) {
 
 func TestSumAllTails(t *testing.T) {
 
-	t.Run("make the sum of some slice", func(t *testing.T) {
-		got := SumAllTails([]int{1,2}, []int{0,9})
-		want := []int{2,9}
-	
+	checkSum := func(t *testing.T, got, want []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("make the sum of some slice", func(t *testing.T) {
+		got := SumAllTails([]int{1,2}, []int{0,9})
+		want := []int{2,9}
+		checkSum(t, got, want)
 	})
 	
 	t.Run("safely sum empty slice", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3,4,5})
 		want := []int{0,9}
-	
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSum(t, got, want)
 	})
 }
