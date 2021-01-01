9a5f83d6a890c3d3a41733150c8c7043e2be3ced
Ilya Petrenko <gr0x562@gmail.com>
2021-01-01 14:30:20 +0300

    add russian

--- helloWorld/helloWorld.go
+++ helloWorld/helloWorld.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 const spanish = "sp"
+const russian = "ru"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const russianHelloPrefix = "Привет, "
 
 func Hello(name, language string) string {
 	if name == "" {
@@ -13,6 +15,10 @@ func Hello(name, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+
+	if language == russian {
+		return russianHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
