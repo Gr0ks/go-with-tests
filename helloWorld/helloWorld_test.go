9b51f911eb801be3ac89a02536231edcd83e8ab7
Ilya Petrenko <gr0x562@gmail.com>
2021-01-01 13:56:08 +0300

    test hello to people

--- helloWorld/helloWorld_test.go
+++ helloWorld/helloWorld_test.go
@@ -4,6 +4,15 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	t.Run("Testing Hello to Ilya", func(t *testing.T) {
+		got := Hello("Ilya")
+		want := "Hello, Ilya"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 	t.Run("Test without arguments", func(t *testing.T) {
 		got := Hello()
 		want := "Hello, world"
