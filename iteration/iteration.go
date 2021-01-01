d5d11e4d9dca8f38facad39a9fc6d66b32796cac
Ilya Petrenko <gr0x562@gmail.com>
2021-01-01 22:24:44 +0300

    Repeater refactoring

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,7 +1,9 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(letter string) (repeated string) {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += letter
 	}
 	return
