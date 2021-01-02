36884c8e0d0b32fb96825e6058ca4571f5edcb2f
Ilya Petrenko <gr0x562@gmail.com>
2021-01-02 14:36:27 +0300

    wallet refactoring

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,13 +1,25 @@
 package pointer
 
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
-	w.balance +=amount
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
