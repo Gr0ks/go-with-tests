1be3002771605c174862a714262a15af8213fba9
Ilya Petrenko <gr0x562@gmail.com>
2021-01-06 14:27:58 +0300

    test unknown map search

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is jast a test"}
 
-	got := dictionary.Search("test")
-	want := "this is jast a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is jast a test"
+	
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(t, got, want)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
