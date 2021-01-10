9bbbec8899cd8664cd141a047046e760f73b8274
Ilya Petrenko <gr0x562@gmail.com>
2021-01-10 20:46:40 +0300

    other use injection

--- dependencyInjection/di.go
+++ dependencyInjection/di.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func GreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
 	Greet(os.Stdout, "Ilya")
+	http.ListenAndServe(":5000", http.HandlerFunc(GreeterHandler))
 }
