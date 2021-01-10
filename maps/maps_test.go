bc34d30fce15a79f69ca87d91d167ec6288a0fa3
Ilya Petrenko <gr0x562@gmail.com>
2021-01-10 12:42:56 +0300

    test error in map add method

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -20,20 +20,38 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-
-	dictionary.Add(word, definition)
-
-	assertDefinition(t, dictionary, word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+	
+		err := dictionary.Add(word, definition)
+	
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+	
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+	
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertError(t *testing.T, got, want error) {
 	t.Helper()
-
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
+	}
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
 	}
 }
 
