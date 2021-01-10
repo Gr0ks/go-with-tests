0a6b3666c0ac338446279dd29671374870f0f76e
Ilya Petrenko <gr0x562@gmail.com>
2021-01-10 23:06:03 +0300

    done mocking/Countdown configurable sleeper

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -14,15 +14,13 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct {}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 type ConfigurableSleeper struct {
 	duration time.Duration
 	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -36,6 +34,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
