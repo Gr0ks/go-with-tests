33c3715c69920b77c450b57bf9b8ac3e5cce584f
Ilya Petrenko <gr0x562@gmail.com>
2021-01-12 22:51:48 +0300

    refactoring select/Racer test

--- select/select_test.go
+++ select/select_test.go
@@ -1,10 +1,25 @@
 package selectRacer
 
-import "testing"
+import (
+	"testing"
+	"net/http/httptest"
+	"time"
+	"net/http"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://wwwquii.co.uk"
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
