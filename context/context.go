32b98cfd3e2ea1b7ca7917c73bc356e85f154312
Ilya Petrenko <gr0x562@gmail.com>
2021-02-08 23:35:10 +0300

    refactoring context test passed

--- context/context.go
+++ context/context.go
@@ -3,6 +3,7 @@ package context
 import (
 	"net/http"
 	"context"
+	"fmt"
 )
 
 type Store interface {
@@ -11,6 +12,7 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		
+		data, _ := store.Fetch(r.Context())
+		fmt.Fprint(w, data)
 	}
 }
