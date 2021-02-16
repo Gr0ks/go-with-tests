191ff04da0e64a47377033e53d0ce7c40d62dbfa
Ilya Petrenko <gr0x562@gmail.com>
2021-02-16 09:16:41 +0300

    test roman 10

--- propertyBasedTests/romanNumerals_test.go
+++ propertyBasedTests/romanNumerals_test.go
@@ -17,6 +17,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"5 gets converted to V", 5, "V"},
 		{"8 gets converted to VIII", 8, "VIII"},
 		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
 	}
 
 	for _, test := range cases {
