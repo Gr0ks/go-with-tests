c7d786ac3464a7b5509a6ae00dca2291e89e3183
Ilya Petrenko <gr0x562@gmail.com>
2021-02-25 09:23:57 +0300

    done I-IV convert to arabic

--- propertyBasedTests/romanNumerals.go
+++ propertyBasedTests/romanNumerals.go
@@ -9,7 +9,18 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral {
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+	return 0
+}
+
+var allRomanNumerals = RomanNumerals {
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -39,5 +50,26 @@ func ConvertToRoman(arabic int) string {
 }
 
 func ConvertToArabic(roman string) int {
-	return 1
+	total := 0
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if i+1 < len(roman) && symbol == 'I' {
+			nextSymbol := roman[i+1]
+
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
 }
